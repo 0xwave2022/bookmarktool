63536c0e38b6e7ffd68c954cb144df4a73ac1e83
0xwave2022 <118503699+0xwave2022@users.noreply.github.com>
2023-04-23 14:09:59 +0800

    Create main.cpp
    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include bookmarktool.h
+#include extern.h
+#include include.h
+#include dbinterface.h
+#include define.h
+
+
+//main.cpp now
+
+in main(argc a, argv b)
+{
+   int x,y=0;
+   if(a>2) 
+     printf("argc parms is a=%d\n",a);
+}
